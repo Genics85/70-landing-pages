06f15cef7e6d874fe1bbcf3c392dfdcdd9f765fb
Genics85 <eugeneamo85@gmail.com>
2023-06-06 00:28:26 +0000

    welcome page responsiveness done

--- src/pages/academy/Welcome.tsx
+++ src/pages/academy/Welcome.tsx
@@ -2,11 +2,11 @@ import welcome from "../../assets/images/welcome.png";
 
 function Welcome() {
   return (
-    <main className="p-20 min-h-screen bg-academyBg">
-      <div className="container flex flex-col-reverse justify-between md:flex-row md:space-x-4 space-y-4">
+    <main className="p-4 md:p-20 min-h-full bg-academyBg">
+      <div className="container gap-y-8 flex flex-col-reverse justify-between md:flex-row md:space-x-4 ">
         <div className="flex flex-col justify-center items-start space-y-4 md:w-2/5">
           <p className="text-sm text-textGrey">MODERN</p>
-          <h4 className="text-6xl font-bold">Education <br/> Academy</h4>
+          <h4 className="text-5xl font-bold">Education <br/> Academy</h4>
           <p className="text-textGrey">
             Laboris laborum velit do aute proident deserunt ullamco dolore magna
             elit id qui officia.
