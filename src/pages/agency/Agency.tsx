b230a853deefd6784e4022c0da6214fc0a7dd449
Genics85 <eugeneamo85@gmail.com>
2023-06-15 17:28:51 +0000

    done with footer

--- src/pages/agency/Agency.tsx
+++ src/pages/agency/Agency.tsx
@@ -1,25 +1,27 @@
-import Contact from './Contact'
-import Features from './Features'
-import Home from './Home'
-import Navigation from './Navigation'
-import Portfolio from './Portfolio'
-import Service from './Service'
-import Team from './Team'
-import Testimonials from './Testimonials'
+import Contact from "./Contact";
+import Features from "./Features";
+import Footer from "./Footer";
+import Home from "./Home";
+import Navigation from "./Navigation";
+import Portfolio from "./Portfolio";
+import Service from "./Service";
+import Team from "./Team";
+import Testimonials from "./Testimonials";
 
 function Agency() {
   return (
     <div>
-        <Navigation/>
-        <Home/>
-        <Service/>
-        <Features/>
-        <Portfolio/>
-        <Testimonials/>
-        <Team/>
-        <Contact/>
+      <Navigation />
+      <Home />
+      <Service />
+      <Features />
+      <Portfolio />
+      <Testimonials />
+      <Team />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default Agency
+export default Agency;
